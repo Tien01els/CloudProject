0d263ab4d705fde65509a8aaaaf1debcd0feb079
Tien01els <tien140801@gmail.com>
2021-12-04 18:37:47 +0700

    Update active form edit
    Update active form edit

--- Frontend/assets/scripts/main.js
+++ Frontend/assets/scripts/main.js
@@ -1,7 +1,7 @@
 const btnMenu = document.querySelector('.btn-menu');
 const headerLeft = document.querySelector('.header-left');
 const main = document.querySelector('#main');
-const btnEdit = document.querySelector('.btn-edit');
+const btnEdits = document.querySelectorAll('.btn-edit');
 const formEdit = document.querySelector('#form-edit');
 const btnCloseForm = document.querySelector('.form-close')
 
@@ -9,9 +9,11 @@ btnMenu.onclick = () => {
     main.classList.toggle('mini-sidebar');
 }
 
-btnEdit.onclick = () => {
-    formEdit.classList.add('active');
-}
+btnEdits.forEach(btnEdit => {
+    btnEdit.onclick = () => {
+        formEdit.classList.add('active');
+    }
+})
 btnCloseForm.onclick = () => {
     formEdit.classList.remove('active');
 }
