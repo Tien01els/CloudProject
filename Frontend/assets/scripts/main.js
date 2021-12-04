8dac3f4f3a834743004c04740275b92fc06187a9
Tien01els <tien140801@gmail.com>
2021-12-04 18:30:50 +0700

    Update fe list students and teachers
    Update fe list students and teachers

--- Frontend/assets/scripts/main.js
+++ Frontend/assets/scripts/main.js
@@ -1,8 +1,17 @@
 const btnMenu = document.querySelector('.btn-menu');
 const headerLeft = document.querySelector('.header-left');
-const sidebar = document.querySelector('#sidebar');
+const main = document.querySelector('#main');
+const btnEdit = document.querySelector('.btn-edit');
+const formEdit = document.querySelector('#form-edit');
+const btnCloseForm = document.querySelector('.form-close')
 
 btnMenu.onclick = () => {
-    console.log('a');
-    sidebar.classList.toggle('mini-sidebar');
+    main.classList.toggle('mini-sidebar');
+}
+
+btnEdit.onclick = () => {
+    formEdit.classList.add('active');
+}
+btnCloseForm.onclick = () => {
+    formEdit.classList.remove('active');
 }
