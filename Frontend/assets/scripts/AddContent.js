0a7e3c20ced5a0b3b57ac252ebe915a86dd2a859
Tien01els <tien140801@gmail.com>
2021-12-06 04:05:35 +0700

    Update ajax
    Update ajax

--- Frontend/assets/scripts/AddContent.js
+++ Frontend/assets/scripts/AddContent.js
@@ -1,5 +1,6 @@
 const btnSubmit = document.querySelector('.btn-submit');
 const inputs = document.querySelectorAll('.form-input');
+
 btnSubmit.onclick = (e) => {
     e.preventDefault();
     var value = {};
@@ -12,15 +13,17 @@ btnSubmit.onclick = (e) => {
 }
 
 
-function addObject(Objects) {
-    $.post("api", Objects)
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function() {
-            alert("error");
-        })
-        .always(function() {
-            alert("complete");
-        });
+function addObjects(Objects) {
+    const api = "http://localhost:8081/student/";
+    console.log(Objects);
+    // $.post("api", Objects)
+    //     .done(function(data) {
+    //         console.log(data);
+    //     })
+    //     .fail(function() {
+    //         alert("error");
+    //     })
+    //     .always(function() {
+    //         alert("complete");
+    //     });
 }
--- Frontend/assets/scripts/main.js
+++ Frontend/assets/scripts/main.js
@@ -1,37 +1,6 @@
 const btnMenu = document.querySelector('.btn-menu');
 const main = document.querySelector('#main');
 
-
 btnMenu.onclick = () => {
     main.classList.toggle('mini-sidebar');
-}
-
-
-
-function getObject() {
-    // var valueObjects = Objects.map(Object => ({ Object: Object.value }));
-    $.post("api")
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function() {
-            alert("error");
-        })
-        .always(function() {
-            alert("complete");
-        });
-}
-
-function addObject(Objects) {
-    var valueObjects = Objects.map(Object => ({ Object: Object.value }));
-    // $.post("api", {...valueObjects })
-    //     .done(function(data) {
-    //         console.log(data);
-    //     })
-    //     .fail(function() {
-    //         alert("error");
-    //     })
-    //     .always(function() {
-    //         alert("complete");
-    //     });
 }
