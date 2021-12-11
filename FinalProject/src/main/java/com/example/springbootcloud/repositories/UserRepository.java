415ba1488eb39f3ebde0fbae78b290cb93c68abc
Quốc Thắng <quocthang036@gmail.com>
2021-12-12 03:52:21 +0700

    Cleaning Project
    Cleaning Project

--- FinalProject/src/main/java/com/example/springbootcloud/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootcloud.repositories;
-
-import com.example.springbootcloud.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
