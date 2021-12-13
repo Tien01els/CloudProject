e0da73b0dc9e77691d91e021dc901bf91c830d3f
Tien01els <tien140801@gmail.com>
2021-12-14 05:10:41 +0700

    Update front end
    Update front end

--- Frontend/assets/scripts/Register.js
+++ Frontend/assets/scripts/Register.js
@@ -1,46 +1,28 @@
 import { URL } from './URL.js';
 
-function checkpass() {
-    if (document.getElementById("id_pass").value !== document.getElementById("id_repass").value) {
-        alert("Password does not match!!!");
-        return false;
-    }
-}
+const btnAdd = document.querySelector(".btn-submit");
 
-function getRole() {
-    var ele = document.getElementsByName("role");
-    for (let i = 0; i < ele.length; ++i) {
-        if (ele[i].checked)
-            return ele[i].value;
-    }
-}
-
-const btnAdd = document.getElementById("btn_register");
 btnAdd.addEventListener("click", (Event) => {
     Event.preventDefault();
 
-    if (checkpass() == false)
-        return;
-
     var Model = {
-        username: document.getElementById("id_user").value,
-        password: document.getElementById("id_pass").value,
-        role: getRole(),
+        username: document.getElementById("username").value,
+        password: 1,
+        role: document.getElementById("list-type").value,
     };
 
-    console.log("testvalue: " + Model);
     var requestJSON = JSON.stringify(Model);
 
     $.ajax({
         type: 'POST',
-        url: URL + '/account/' + getRole(),
+        url: URL + '/account/' + Model.role,
         dataType: 'json',
         headers: {
             "Content-Type": "application/json",
         },
         data: requestJSON,
         success: function(data) {
-            location.href = "Login.html";
+            console.log("Create account success");
         },
         error: function() {
             console.log("The following error occured: ");
